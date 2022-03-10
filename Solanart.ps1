3fd660ea67299e2777f0dc3f4215ffd5ddfd88d5
VirusBlade <73291752+VirusBlade@users.noreply.github.com>
2022-03-10 17:49:09 +0100

    Add files via upload

---
+++ Solanart.ps1
@@ -0,0 +1,11 @@
+﻿$CollectionName = "roguesharks"
+$HTML = Invoke-RestMethod "https://api.solanart.io/get_nft?collection=$CollectionName&page=0&limit=20&order=price-ASC&min=0&max=99999&search=&listed=true&fits=all&bid=all"
+$i=-1
+$All =@()
+do
+{
+    $i++
+    $All += Invoke-RestMethod "https://api.solanart.io/get_nft?collection=$CollectionName&page=$i&limit=20&order=price-ASC&min=0&max=99999&search=&listed=true&fits=all&bid=all"
+}
+until ($i -eq $html.pagination.maxPages)
+$all.items | ft
